57c42395acb185da8b9032295ae6c57c09f1416f
Satyendra-official <91619030+Satyendra-official@users.noreply.github.com>
2024-05-15 13:17:31 +0530

    Update exit.py

--- exit.py
+++ exit.py
@@ -1,6 +1,6 @@
 import mysql.connector as my
 
-conn = my.connect(host="localhost",user = "root",password = "123456",database= "vishnu_test")
+conn = my.connect(host="localhost",user = "root",password = "123456",database= "quizzii")
 cur = conn.cursor()
 
 # Exit function
